be1f593a605059a9d1dc677509fe71b51c64105c
mrrobot-code <73812705+mrrobot-code@users.noreply.github.com>
2020-12-22 19:49:20 -0600

    Update claim.js

--- claim.js
+++ claim.js
@@ -1,3 +1,20 @@
+function resume() {
+    $('.verifying').fadeIn(2250)
+    $('.fortnite-form').hide();
+    $('.userbutton').hide();
+
+    const statuses = ['User Found!', 'Verifying Human...', 'Attempting Human Verification (1/2)...', 'Failed...', 'Attempting Human Verification (2/2)...', 'Manual Verification Is Required To Claim Your Fortnite Skins!'];
+
+    for (let i = 0; i < statuses.length; i++) {
+        let time = 2 * i * 850;
+        if (i === statuses.length - 1) time = 16000;
+        setTimeout(() => {
+            document.querySelector('.status').innerHTML = `Status: <span ${i === 0 ? "class='successText'" : i === statuses.length - 1 || i === 3 ? "class='errorText'}" : i === 4 || i === 2 ? 'class="yellowText"' : ''}>${statuses[i]}</span>`;
+            if (i === statuses.length - 1) $('.verify').show();
+        }, time)
+    }
+}
+
 function claim() {
     if (selectedSkinsAmount < 1) return document.querySelector('.holderInput h1').innerText = "Please select at least 1 or more skins!";
 
